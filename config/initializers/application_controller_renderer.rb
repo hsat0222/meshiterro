e648e7e69889cd0537380425cd97c20c71c95184
hsat0222 <hsat2222@gmail.com>
2019-10-15 18:56:53 +0900

    [finish]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e7849146165642f0bc89c706fc41e9236807dc540b29e53c4037dc48e09b8061382877f8d7f0e1d810ae1a5d669b926b08f12d353cfc542da3d12e5c1f4b7a84'
